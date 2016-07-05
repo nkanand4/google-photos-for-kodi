a11ce694abadc85efba9a616b90e876a6d6d4090
Nitesh Anand <nkanand4@gmail.com>
2016-07-04 23:48:36 -0500

    Able to fetch the list of albums along with photos in them.

--- google-photos.py
+++ google-photos.py
@@ -1,6 +1,7 @@
 import json
 import urllib2
 import urllib
+import xml.dom.minidom
 
 __author__ = 'nitesh'
 
@@ -49,10 +50,10 @@ def get_access_token_using_refresh_token():
     print(json_obj)
 
 
-def make_request(req):
+def make_request(req, data=None):
     req.add_header('Authorization', 'Bearer ' + access_token)
     try:
-        resp_stream = urllib2.urlopen(req)
+        resp_stream = urllib2.urlopen(req, data)
         response = resp_stream.read()
         return response
     except urllib2.HTTPError, e:
@@ -60,6 +61,9 @@ def make_request(req):
             get_access_token_using_refresh_token()
             response = make_request(req)
             return response
+        elif e.code == 403:
+            get_user_code_for_device()
+            return ""
 
 
 def get_user_profile_info():
@@ -70,15 +74,70 @@ def get_user_profile_info():
     print name
 
 
+def get_text(nodelist):
+    rc = []
+    for node in nodelist:
+        if node.nodeType == node.TEXT_NODE:
+            rc.append(node.data)
+    return ''.join(rc)
+
+
+def parse_xml_for_albums(xmlstr):
+    entries = []
+    dom = xml.dom.minidom.parseString(xmlstr)
+    feed = dom.getElementsByTagName('feed')
+    if len(feed) > 0:
+        feed = feed[0]
+        entryNodes = feed.getElementsByTagName('entry')
+        for entryNode in entryNodes:
+            idNode = entryNode.getElementsByTagName('gphoto:id')
+            titleNode = entryNode.getElementsByTagName('title')
+            if len(idNode):
+                album_id = get_text(idNode[0].childNodes)
+                title = get_text(titleNode[0].childNodes)
+                entries.append({'id': album_id, 'title': title})
+
+    print entries
+    return entries
+
+
 def get_user_album_list():
     req = urllib2.Request("https://picasaweb.google.com/data/feed/api/user/default")
     xml_string = make_request(req)
-    print(xml_string)
+    album_list = parse_xml_for_albums(xml_string)
+    for album in album_list:
+        print album['title']
 
+
+def parse_xml_for_photos(xmlstr):
+    photos = []
+    dom = xml.dom.minidom.parseString(xmlstr)
+    feed = dom.getElementsByTagName('feed')
+    if len(feed) > 0:
+        feed = feed[0]
+        entryNodes = feed.getElementsByTagName('entry')
+        for entryNode in entryNodes:
+            idNode = entryNode.getElementsByTagName('content')
+            titleNode = entryNode.getElementsByTagName('title')
+            if len(idNode):
+                url = idNode[0].getAttribute('src')
+                title = get_text(titleNode[0].childNodes)
+                photos.append({'url': url, 'title': title})
+
+    return photos
+
+
+def get_photo_list_for_album(album_id):
+    req = urllib2.Request("https://picasaweb.google.com/data/feed/api/user/default/albumid/" + album_id + '?imgmax=d')
+    xml_string = make_request(req)
+    photo_list = parse_xml_for_photos(xml_string)
+    for photo in photo_list:
+        print photo['url']
 
 
 #get_user_code_for_device()
 #get_tokens_for_user()
 #get_user_profile_info()
-get_user_album_list()
 #get_access_token_using_refresh_token()
+#get_user_album_list()
+#get_photo_list_for_album('')
